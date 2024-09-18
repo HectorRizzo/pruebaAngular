57e54e12ccb1a5aac401c9979ba4e2b7680198d5
Hector Rizzo <daymorrizzo@gmail.com>
2024-09-18 01:18:41 -0500

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },  // Cambia esto a tu componente principal
+    {
+      path: 'about',
+      loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+    },
+    { path: '**', redirectTo: '/home' }  // Ruta comodín para redirigir a la página principal
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule],
+    providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
+  })
+  export class AppRoutingModule { }
